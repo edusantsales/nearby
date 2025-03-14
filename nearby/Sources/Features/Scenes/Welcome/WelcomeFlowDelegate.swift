8b289cde50e929a2721f54590827cf33f43d88d6
Eduardo Sales <edusantsales@gmail.com>
2025-03-14 10:47:05 -0300

    style: Formating files with swift-format feat: Added navigation to the Home View

---
+++ nearby/Sources/Features/Scenes/Welcome/WelcomeFlowDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  WelcomeFlowDelegate.swift
+//  Nearby
+//
+//  Created by Eduardo Sant Ana Sales on 27/02/25.
+//
+
+import Foundation
+
+public protocol WelcomeFlowDelegate: AnyObject {
+    func goToHome()
+}
