f81ffafe32ca7704cbffa8e4a5c4bd83a8310204
Eduardo Sant'Ana <edusantsales@gmail.com>
2026-07-10 11:04:30 -0300

    chore(project): Initial commit

---
+++ nearby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nearby
+//
+//  Created by Eduardo Sant Ana Sales on 10/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nearby/NearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  nearbyApp.swift
+//  nearby
+//
+//  Created by Eduardo Sant Ana Sales on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct nearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
